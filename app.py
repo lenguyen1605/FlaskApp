f65ccba0dbb7084a379e9b81a56d8c46d210a0ee
lenguyen1605 <nguyennhatle1605@gmail.com>
2021-07-26 10:03:53 +0700

    First commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import *
+from flask import url_for
+import argparse
+import requests
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/number-cats')
+def main():
+    number = request.args.get('number', type=int)
+    directory = "static"
+
+    parent_dir = '/Users/lenguyen/Desktop/'
+    path = os.path.join(parent_dir, directory)
+
+    if not os.path.exists(path):
+        os.mkdir(path)
+    urls = []
+    for i in range(number):
+        location = os.path.join(path, 'cat' + str(i))
+
+        r = requests.get('https://cataas.com/cat')
+        with open(location, 'wb') as f:
+            f.write(r.content)
+        urls.append(url_for('static', filename='cat' + str(i)))
+        
+
+    return tuple(urls)
+
+
+if __name__ == "__main__":
+    app.run()
