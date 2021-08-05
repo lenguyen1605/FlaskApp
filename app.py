0606802ba106099225df232c934e9b22ec693ce7
lenguyen1605 <84549731+lenguyen1605@users.noreply.github.com>
2021-08-05 09:01:47 +0700

    Update app.py
    Put static folder inside project folder

--- app.py
+++ app.py
@@ -7,28 +7,27 @@ import os
 app = Flask(__name__)
 
 
-@app.route('/number-cats')
-def main():
+@app.route('/number-cats/<int:number>')
+def main(number):
     urls = []
-    number = request.args.get('number', type=int)
     directory = "static"
 
-    parent_dir = '/Users/lenguyen/Desktop/'
+    parent_dir = '/Users/lenguyen/Desktop/ProjectFlask'
     path = os.path.join(parent_dir, directory)
 
     if not os.path.exists(path):
         os.mkdir(path)
     
     for i in range(number):
-        location = os.path.join(path, 'cat' + str(i))
+        location = os.path.join(path, 'cat' + str(i) + '.jpg')
 
         r = requests.get('https://cataas.com/cat')
         with open(location, 'wb') as f:
             f.write(r.content)
-        urls.append(url_for('static', filename='cat' + str(i)))
+        urls.append(url_for('static', filename='cat' + str(i) + '.jpg', _external=True))
         
 
-    return str(tuple(urls))
+    return str(urls)
 
 
 if __name__ == "__main__":
